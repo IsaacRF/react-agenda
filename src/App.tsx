387b5ec802945480a7748fa7e4f29bb6c79fc092
Isaac R.F <isaacrf239@gmail.com>
2021-03-17 09:16:33 +0100

    Add a Form component with basic structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { Fragment } from "react";
 
-function App() {
-  return (
+import Form from "./components/Form";
+
+const App = () => (
+  <Fragment>
     <h1>React Agenda</h1>
-  );
-}
+    <div className="container">
+      <div className="one-half column">
+        <Form />
+      </div>
+      <div className="one-half column">
+        2
+      </div>
+    </div>
+  </Fragment>
+);
 
 export default App;
---
+++ src/components/Form/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export interface FormProps {
+
+};
+
+const Form: React.FC<FormProps> = () => (
+    <h1>-FORM COMPONENT-</h1>
+);
+
+export default Form;
