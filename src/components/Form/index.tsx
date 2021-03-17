d325635faaa78e4665e1a99b3c1d5cce3063e895
Isaac R.F <isaacrf239@gmail.com>
2021-03-17 09:35:26 +0100

    Add fields to Form component

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,11 +1,53 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 export interface FormProps {
 
 };
 
 const Form: React.FC<FormProps> = () => (
-    <h1>-FORM COMPONENT-</h1>
+    <Fragment>
+        <h2>Create appointment</h2>
+
+        <form>
+            <label>Title</label>
+            <input
+                type="text"
+                name="title"
+                className="u-full-width"
+                placeholder="Appointment title"
+            />
+            <label>Invitees</label>
+            <input
+                type="text"
+                name="invitees"
+                className="u-full-width"
+                placeholder="Invitees separated by comma"
+            />
+            <label>Date</label>
+            <input
+                type="date"
+                name="date"
+                className="u-full-width"
+            />
+            <label>Time</label>
+            <input
+                type="time"
+                name="time"
+                className="u-full-width"
+            />
+            <label>Description</label>
+            <textarea
+                name="description"
+                className="u-full-width"
+                placeholder="Appointment description"
+            />
+
+            <button
+                type="submit"
+                className="u-full-width button-primary"
+            >Schedule</button>
+        </form>
+    </Fragment>
 );
 
 export default Form;
